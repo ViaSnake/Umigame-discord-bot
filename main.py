d39cd47188b158ebc63b48afbf280f303e335aa5
ethernetcat <ethernetcat@protonmail.com>
2020-09-08 17:46:05 +0900

    Improve

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import discord, os
 
-client = discord.Client()
-
-@client.event
+@bot.event
 async def on_message(message):
   if message.channel.name == "ウミガメのスープ":
-    if message.content[-1] == "?" or message.content[-1] == "？":
+    if message.content.endswith("?") or message.content.endswith("？"):
       await message.add_reaction("⭕")
       await message.add_reaction("❌")
       await message.add_reaction("🤔")
-      
-    if message.content[0] == "?":
-      if message.content[1:] == "about":
-        embed=discord.Embed(title="ウミガメのスープbotについて", description="メッセージの最後が「?」もしくは「？」である時に、リアクションで「⭕」「❌」「🤔」が付与されます。「⭕」はYESの意味。「❌」はNOの意味。「🤔」は分からない/関係ないの意味。ウミガメのスープの意味はggrks")
-        await message.channel.send(embed=embed)
+    elif message.content.endswith("。"):
+      await message.add_reaction("🆗")
+      await message.add_reaction("🆖")
+    elif message.content == "?about":
+      embed=discord.Embed(title="ウミガメのスープbotについて", description="メッセージの最後が「?」もしくは「？」である時に、リアクションで「⭕」「❌」「🤔」が付与されます。「⭕」はYESの意味。「❌」はNOの意味。「🤔」は分からない/関係ないの意味。回答を行いたい場合は、文章の最後に「。」をつけることで「:ok:」「:ng:」がリアクションとして付与される。「:ok:」は正解の意味。「:ng:」は不正解の意味")
+      await message.channel.send(embed=embed)
 
 client.run(os.environ["TOKEN"])
