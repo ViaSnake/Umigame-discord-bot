e115c7a80a72d243601f3e8eea278660e83c650c
ethernetcat <ethernetcat@protonmail.com>
2020-09-09 15:58:05 +0900

    Fix typo in about command

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ async def help(ctx):
 
 @bot.command()
 async def about(ctx):
-    embed=discord.Embed(title="ウミガメのスープbot", description="")
-    embed=embed.add_field(name="問題提示方法", value="メッセージが「?q」で始まる時に自動的に問題と認識されます。\n例： `?q これは問題です。`", inline=False)
+    embed=discord.Embed(title="ウミガメのスープbot")
+    embed=embed.add_field(name="問題出題方法", value="メッセージが「?q」で始まる時に自動的に問題と認識されます。\n例： `?q これは問題です。`", inline=False)
     embed=embed.add_field(name="質問方法", value="メッセージが「?」もしくは「？」で終わる時に、自動的にリアクションで「⭕」「❌」「🤔」が付与されます。\n「⭕」はYESの意味。「❌」はNOの意味。「🤔」は分からない/関係ないの意味。\n例：`これは質問です？` `This is question?`", inline=False)
     embed=embed.add_field(name="回答方法", value="回答を行いたい場合は、文章の最後に「。」をつけることで「:ok:」「:ng:」がリアクションとして付与される。「:ok:」は正解の意味。「:ng:」は不正解の意味", inline=False)
     await ctx.send(embed=embed)
