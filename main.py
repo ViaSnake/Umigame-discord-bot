782434ea86a5f6e6749c9b3fc176cabddb76956b
ethernetcat <ethernetcat@protonmail.com>
2020-09-10 15:33:49 +0900

    Change bot name

--- main.py
+++ main.py
@@ -23,13 +23,13 @@ async def on_message_delete(message):
 
 @bot.command()
 async def help(ctx):
-    embed=discord.Embed(title="ウミガメのスープbot", description="?help - ヘルプ\n?about - 情報\n?q [question] - 問題出題")
+    embed=discord.Embed(title="ウミガメbot", description="?help - ヘルプ\n?about - 情報\n?q [question] - 問題出題")
     await ctx.send(embed=embed)
     await ctx.message.delete()
 
 @bot.command()
 async def about(ctx):
-    embed=discord.Embed(title="ウミガメのスープbot")
+    embed=discord.Embed(title="ウミガメbot")
     embed=embed.add_field(name="問題出題方法", value="メッセージが「?q」で始まる時に自動的に問題と認識されます。\n例： `?q これは問題です。`", inline=False)
     embed=embed.add_field(name="質問方法", value="メッセージが「?」もしくは「？」で終わる時に、自動的にリアクションで「⭕」「❌」「🤔」が付与されます。\n「⭕」はYESの意味。「❌」はNOの意味。「🤔」は分からない/関係ないの意味。\n例：`これは質問です？` `This is question?`", inline=False)
     embed=embed.add_field(name="回答方法", value="回答を行いたい場合は、文章の最後に「。」をつけることで「:ok:」「:ng:」がリアクションとして付与される。「:ok:」は正解の意味。「:ng:」は不正解の意味", inline=False)
