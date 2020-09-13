ae43e369c0859e999b35fe86f707759ae4e3a2f0
ethernetcat <ethernetcat@protonmail.com>
2020-09-13 21:20:29 +0900

    Send question in question channel

--- main.py
+++ main.py
@@ -41,13 +41,14 @@ async def q(ctx, *, arg):
     category = message.guild.get_channel(category_id)
     new_channel = await category.create_text_channel(name=channel_name)
     return new_channel
-  
+
   embed=discord.Embed(title="問題", description=arg, color=0x00ff00)
   embed.set_author(name="{} が問題を作成".format(ctx.author))
   await ctx.send(embed=embed)
-
   new_channel = await create_text_channel(ctx.message, channel_name=str(uuid.uuid4())[-6:])
-  await ctx.send(f'{new_channel.mention} を作成しました。')
+  await ctx.send(f'{new_channel.mention} を作成しました')
+  channel = bot.get_channel(new_channel.id)
+  await channel.send(embed=embed)
   await ctx.message.delete()
 
 @bot.command()
